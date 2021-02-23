a103cecc2e8d274985322a8e7d78cd11cbc9354a
Liping Marlow <limarlow@microsoft.com>
2021-02-23 12:18:34 -0600

    add access to keyvault

--- Startup.cs
+++ Startup.cs
@@ -31,13 +31,27 @@ namespace keyvault
 
             app.UseRouting();
 
-            
+            SecretClientOptions options = new SecretClientOptions()
+                {
+                    Retry =
+                    {
+                        Delay= TimeSpan.FromSeconds(2),
+                        MaxDelay = TimeSpan.FromSeconds(16),
+                        MaxRetries = 5,
+                        Mode = RetryMode.Exponential
+                    }
+                };
+            var client = new SecretClient(new Uri("https://lpmsiauth.vault.azure.net/"), new DefaultAzureCredential(),options);
+
+            KeyVaultSecret secret = client.GetSecret("connectionString");
+
+            string secretValue = secret.Value;
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World!"+ secretValue);
                 });
             });
         }
